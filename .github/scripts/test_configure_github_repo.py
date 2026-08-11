b60c246da69332beb7c9580bdce38036b67221a7
Joshua Gilman <joshuagilman@gmail.com>
2026-08-10 20:08:46 -0700

    chore: reshape template into library-only project (#8)

--- .github/scripts/test_configure_github_repo.py
+++ .github/scripts/test_configure_github_repo.py
@@ -102,8 +102,8 @@ build_type = "branch"
 
         plan = configure.build_plan(
             api,
-            "meigma",
-            "template-go",
+            "imgoci",
+            "go-oci-blob",
             base_config({"build_type": "workflow", "https_enforced": True}),
             mode="plan",
             hostname="github.com",
@@ -119,8 +119,8 @@ build_type = "branch"
 
         plan = configure.build_plan(
             api,
-            "meigma",
-            "template-go",
+            "imgoci",
+            "go-oci-blob",
             base_config({"build_type": "workflow", "https_enforced": True}),
             mode="plan",
             hostname="github.com",
@@ -133,7 +133,7 @@ build_type = "branch"
     def test_apply_create_pages_runs_follow_up_update(self) -> None:
         api = FakeGitHubApi()
         plan = configure.PlanResult(
-            repo="meigma/template-go",
+            repo="imgoci/go-oci-blob",
             hostname="github.com",
             mode="apply",
             changes=[
@@ -153,7 +153,7 @@ build_type = "branch"
             warnings=[],
         )
 
-        applied = configure.apply_plan(api, "meigma", "template-go", plan)
+        applied = configure.apply_plan(api, "imgoci", "go-oci-blob", plan)
 
         self.assertEqual(applied, ["Create GitHub Pages site"])
         self.assertEqual(api.created_pages, [{"build_type": "workflow"}])
